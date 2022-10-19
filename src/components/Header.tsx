9c13ca63a076937feb5b2d8622871cc28278d989
Keven Pacheco <kevenpacheco2000@gmail.com>
2022-10-19 14:18:35 -0300

    fix: change from Ignite Lab text to Ignite Feed text in header

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -6,7 +6,7 @@ export function Header() {
   return (
     <header className={styles.header}>
       <img src={igniteLogo} alt="Logotipo do Ignite" />
-      <p>Ignite Lab</p>
+      <p>Ignite Feed</p>
     </header>
   );
 }
