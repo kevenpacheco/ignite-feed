d75a1c980d7abbcfbc868de1356f5e446e3b2bb6
Keven Pacheco <kevenpacheco2000@gmail.com>
2022-10-19 22:51:00 -0300

    refactor: show list of comments only if there is a comment on the post

--- src/components/Post.tsx
+++ src/components/Post.tsx
@@ -1,18 +1,18 @@
-import { ChangeEvent, FormEvent, InvalidEvent, useState } from 'react';
-import { format, formatDistanceToNow } from 'date-fns';
-import ptBR from 'date-fns/locale/pt-BR';
+import { ChangeEvent, FormEvent, InvalidEvent, useState } from "react";
+import { format, formatDistanceToNow } from "date-fns";
+import ptBR from "date-fns/locale/pt-BR";
 
-import styles from './Post.module.css';
+import styles from "./Post.module.css";
 
-import { PostI } from '../App';
+import { PostI } from "../App";
 
-import { Avatar } from './Avatar';
-import { Comment } from './Comment';
-import { Button } from './Button';
+import { Avatar } from "./Avatar";
+import { Comment } from "./Comment";
+import { Button } from "./Button";
 
 export function Post({ author, publishedAt, content }: PostI) {
   const [comments, setComments] = useState<string[]>([]);
-  const [newCommentText, setNewCommentText] = useState('');
+  const [newCommentText, setNewCommentText] = useState("");
 
   const publishedDateFormatted = format(
     publishedAt,
@@ -20,35 +20,32 @@ export function Post({ author, publishedAt, content }: PostI) {
     { locale: ptBR }
   );
 
-  const publishedDateRelativeToNow = formatDistanceToNow(
-    publishedAt,
-    {
-      locale: ptBR,
-      addSuffix: true
-    },
-  );
+  const publishedDateRelativeToNow = formatDistanceToNow(publishedAt, {
+    locale: ptBR,
+    addSuffix: true,
+  });
 
   function handleNewCommentChange(event: ChangeEvent<HTMLTextAreaElement>) {
-    event.target.setCustomValidity('')
-    setNewCommentText(event.target.value)
+    event.target.setCustomValidity("");
+    setNewCommentText(event.target.value);
   }
 
   function handleNewCommentInvalid(event: InvalidEvent<HTMLTextAreaElement>) {
-    event.target.setCustomValidity('Esse campo é obrigatório!')
+    event.target.setCustomValidity("Esse campo é obrigatório!");
   }
 
   function handleCreateNewComment(event: FormEvent) {
     event.preventDefault();
-    setComments([...comments, newCommentText])
-    setNewCommentText('')
+    setComments([...comments, newCommentText]);
+    setNewCommentText("");
   }
 
   function deleteComment(commentToDelete: string) {
-    const commentsWithoutDeleteOne = comments.filter(comment => {
-      return comment !== commentToDelete
+    const commentsWithoutDeleteOne = comments.filter((comment) => {
+      return comment !== commentToDelete;
     });
 
-    setComments(commentsWithoutDeleteOne)
+    setComments(commentsWithoutDeleteOne);
   }
 
   const isNewCommentEmpty = newCommentText.length === 0;
@@ -57,9 +54,7 @@ export function Post({ author, publishedAt, content }: PostI) {
     <article className={styles.post}>
       <header>
         <div className={styles.author}>
-          <Avatar
-            src={author.avatarUrl}
-          />
+          <Avatar src={author.avatarUrl} />
 
           <div className={styles.authorInfo}>
             <strong>{author.name}</strong>
@@ -76,11 +71,15 @@ export function Post({ author, publishedAt, content }: PostI) {
       </header>
 
       <div className={styles.content}>
-        {content.map(line => {
-          if (line.type === 'paragraph') {
+        {content.map((line) => {
+          if (line.type === "paragraph") {
             return <p key={line.content}>{line.content}</p>;
-          } else if (line.type === 'link') {
-            return <p key={line.content}><a href="">{line.content}</a></p>
+          } else if (line.type === "link") {
+            return (
+              <p key={line.content}>
+                <a href="">{line.content}</a>
+              </p>
+            );
           }
         })}
       </div>
@@ -104,17 +103,19 @@ export function Post({ author, publishedAt, content }: PostI) {
         </footer>
       </form>
 
-      <div className={styles.commentList}>
-        {comments.map(comment => {
-          return (
-            <Comment
-              key={comment}
-              content={comment}
-              onDeleteComment={deleteComment}
-            />
-          )
-        })}
-      </div>
+      {!!comments?.length && (
+        <div className={styles.commentList}>
+          {comments.map((comment) => {
+            return (
+              <Comment
+                key={comment}
+                content={comment}
+                onDeleteComment={deleteComment}
+              />
+            );
+          })}
+        </div>
+      )}
     </article>
   );
 }
